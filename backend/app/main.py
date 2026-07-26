c3419a97c5ca3f8cec475dae7151c95687c9c4fe
Kaukab06 <kaukaberum2006@gmail.com>
2026-07-26 22:23:13 +0530

    Save backend changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.init_db import init_db
 from app.api.product_routes import router
@@ -6,18 +7,20 @@ from app.api.product_routes import router
 app = FastAPI(
     title="KiranaAI API",
     version="1.0.0",
-    description="AI-powered Inventory Management"
 )
 
-# Create Database
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 init_db()
 
-# Register Product APIs
 app.include_router(router)
-
 
 @app.get("/")
 def home():
-    return {
-        "message": "Welcome to KiranaAI Backend 🚀"
-    }
+    return {"message":"Welcome to KiranaAI 🚀"}
